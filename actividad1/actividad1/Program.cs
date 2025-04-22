dc59fbb024c0b282ad7fcd3e0cf2c7933d84404f
Miguel Guevara <miguel.guevara.contreras26@gmail.com>
2025-04-21 17:15:05 -0700

    Agregado el proyecto de consola (Actividad 1 - parte I)

---
+++ actividad1/actividad1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace actividad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos a UMI/Universidad Coppel");
+
+            Console.Write("Nombre completo: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            string edad = Console.ReadLine();
+
+            Console.Write("Fecha de Nacimiento (dd/mm/aaaa): ");
+            string fechaNacimiento = Console.ReadLine();
+
+            Console.Write("Carrera a la que desea ingresar: ");
+            string carrera = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Gracias {nombre} por formar parte de UMI/Universidad Coppel, Bienvenido a la carrera {carrera}.");
+            Console.WriteLine("\nDatos ingresados:");
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Fecha de Nacimiento: {fechaNacimiento}");
+            Console.WriteLine($"Carrera: {carrera}");
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
